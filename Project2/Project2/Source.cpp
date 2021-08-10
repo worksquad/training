1dd26ab3403b438f5854537915e050789ae2c55c
daimonji <leyshkinanton@gmail.com>
2021-08-10 22:19:24 +0300

    shared_ptr -> unique_ptr

--- Project2/Project2/Source.cpp
+++ Project2/Project2/Source.cpp
@@ -2,28 +2,29 @@
 #include <stack>
 #include <vector>
 #include <sstream>
+
 using namespace std;
 
 class Tree
 {
 private:
 	string data;
-	vector<shared_ptr<Tree>> children = vector<shared_ptr<Tree>>();
+	vector<unique_ptr<Tree>> children = vector<unique_ptr<Tree>>();
 public:
 	Tree(string data) { this->data = data; }
 	int childnum() { return children.size(); };
 	string getdata() { return data; }
 	Tree* child(int i) { return children[i].get(); };
 	void print() { cout << data << endl; };
-	void add(shared_ptr<Tree> child) { children.push_back(child); };
+	void add(Tree* child) { children.push_back(unique_ptr<Tree>(move(child))); };
 };
 
 void printall(Tree* root)
 {
-	if (root != NULL)
+	if(root)
 	{
-		int n = root->childnum();
-		for (int i = 0; i < n; i++)	printall(root->child(i));
+		for(int i = 0, n = root->childnum(); i < n; i++)
+			printall(root->child(i));
 		root->print();
 	}
 }
@@ -32,36 +33,45 @@ void prall(Tree* root)
 {
 	stack<Tree*> toprint;
 	toprint.push(root);
-	while (toprint.size() != 0)
+	while(toprint.size() != 0)
 	{
 		Tree* temp = toprint.top();
 		temp->print();
 		toprint.pop();
-		for (int i = 0; i < temp->childnum(); i++) toprint.push(temp->child(i));
+		for(int i = 0; i < temp->childnum(); i++)
+			toprint.push(temp->child(i));
 	}
 }
 
-Tree* createNprint()
+Tree* createNprint(int jPrm, int iPrm)
 {
 	unique_ptr<Tree> root = move(make_unique<Tree>("root"));
 	Tree* temp = root.get();
-	for (int j = 0; j < 3; j++)
+	for(int j = 0; j < jPrm; j++)
 	{
-		for (int i = 0; i < 3; i++)
+		for(int i = 0; i < iPrm; i++)
 		{
 			string tstr = temp->getdata() + " " + to_string(i);
-			shared_ptr<Tree> tmpc = move(make_shared<Tree>((tstr)));
-			temp->add(tmpc);
+			temp->add(new Tree(tstr));
 		}
 		temp = temp->child(j);
 	}
 	temp = root.get();
+
+	cout << "Recursion: " << endl;
 	printall(temp);
+
+	cout << endl << "Simple:" << endl;
 	prall(temp);
+
 	return root.get()->child(1);
 }
 
 int main()
 {
-	Tree * check = createNprint();
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF); // flag for check mem leak
+	Tree* check = createNprint(3, 3);
+	bool bLeak = true;
+	if(bLeak)
+		new Tree("1");
 }
